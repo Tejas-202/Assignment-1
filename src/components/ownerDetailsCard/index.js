1bf9e0ea7746b61bcd919e85816e8010aba154d0
Tejas-coder <tejaskaranjvkar@gmail.com>
2022-02-15 19:24:56 +0530

    update_1

--- src/components/ownerDetailsCard/index.js
+++ src/components/ownerDetailsCard/index.js
@@ -2,97 +2,79 @@ import React from 'react'
 import "./ownerDetailsCard.css"
 import person from "../../assest/person.png";
 
+const ownerdetals = [
+    {
+      id: 1,
+      image: person,
+      name: "Tejal Dipak Karanjavkar",
+      interest: "Aspiring Front End Developer",
+      expertin1: "Software Development Life Cycle (SDLC)",
+      expertin2: "Software dev methodologies",
+      expertin3: "Git Version Control, Github",
+    },
+    {
+        id: 2,
+        image: person,
+        name: "Tejal Dipak Karanjavkar",
+        interest: "Aspiring Front End Developer",
+        expertin1: "Software Development Life Cycle (SDLC)",
+        expertin2: "Software dev methodologies",
+        expertin3: "Git Version Control, Github",
+      },
+      {
+        id: 3,
+        image: person,
+        name: "Tejal Dipak Karanjavkar",
+        interest: "Aspiring Front End Developer",
+        expertin1: "Software Development Life Cycle (SDLC)",
+        expertin2: "Software dev methodologies",
+        expertin3: "Git Version Control, Github",
+      },
+      {
+        id: 4,
+        image: person,
+        name: "Tejal Dipak Karanjavkar",
+        interest: "Aspiring Front End Developer",
+        expertin1: "Software Development Life Cycle (SDLC)",
+        expertin2: "Software dev methodologies",
+        expertin3: "Git Version Control, Github",
+      },
+      {
+          id: 5,
+          image: person,
+          name: "Tejal Dipak Karanjavkar",
+          interest: "Aspiring Front End Developer",
+          expertin1: "Software Development Life Cycle (SDLC)",
+          expertin2: "Software dev methodologies",
+          expertin3: "Git Version Control, Github",
+        },
+  ];
+
 function OwnerDetailsCard() {
   return (
     <div className='max-width container'>
-        <div className='owner-details-card'>
-            <div className='image-name-detilas'>
-                <img className='owner-image' src={person} alt='owner-image' />
-                <div className='name-details'>
-                    <div className='owner-name'>Tejas Dipak Karanjavkar</div>
-                    <div className='owner-interest'>Aspiring Front End Developer</div>
+        {ownerdetals.map((details) => {
+            return(
+                <div className='owner-details-card'>
+                    <div className='image-name-detilas'>
+                        <img className='owner-image' src={details.image} alt='owner-image' />
+                        <div className='name-details'>
+                            <div className='owner-name'>{details.name}</div>
+                            <div className='owner-interest'>{details.interest}</div>
+                        </div>
+                    </div>
+                    <div className='seperation-line'></div>
+                    <div className='learning-details'> 
+                        <div className='haeding'>Top Areas</div>
+                        <div className='expert-list'>
+                            <div className='expert-in'>{details.expertin1}</div>
+                            <div className='expert-in'>{details.expertin2}</div>
+                            <div className='expert-in'>{details.expertin3}</div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className='seperation-line'></div>
-            <div className='learning-details'> 
-                <div className='haeding'>Top Areas</div>
-                <div className='expert-list'>
-                    <div className='expert-in'>Software Development Life Cycle (SDLC)</div>
-                    <div className='expert-in'>Software dev methodologies</div>
-                    <div className='expert-in'>Git Version Control, Github</div>
-                </div>
-            </div>
-        </div>
-
-        <div className='owner-details-card'>
-            <div className='image-name-detilas'>
-                <img className='owner-image' src={person} alt='owner-image' />
-                <div className='name-details'>
-                    <div className='owner-name'>Tejas Dipak Karanjavkar</div>
-                    <div className='owner-interest'>Aspiring Front End Developer</div>
-                </div>
-            </div>
-            <div className='learning-details'> 
-                <div className='haeding'>Top Areas</div>
-                <div className='expert-list'>
-                    <div className='expert-in'>Software Development Life Cycle (SDLC)</div>
-                    <div className='expert-in'>Software dev methodologies</div>
-                    <div className='expert-in'>Git Version Control, Github</div>
-                </div>
-            </div>
-        </div>
-
-        <div className='owner-details-card'>
-            <div className='image-name-detilas'>
-                <img className='owner-image' src={person} alt='owner-image' />
-                <div className='name-details'>
-                    <div className='owner-name'>Tejas Dipak Karanjavkar</div>
-                    <div className='owner-interest'>Aspiring Front End Developer</div>
-                </div>
-            </div>
-            <div className='learning-details'> 
-                <div className='haeding'>Top Areas</div>
-                <div className='expert-list'>
-                    <div className='expert-in'>Software Development Life Cycle (SDLC)</div>
-                    <div className='expert-in'>Software dev methodologies</div>
-                    <div className='expert-in'>Git Version Control, Github</div>
-                </div>
-            </div>
-        </div>
-        <div className='owner-details-card'>
-            <div className='image-name-detilas'>
-                <img className='owner-image' src={person} alt='owner-image' />
-                <div className='name-details'>
-                    <div className='owner-name'>Tejas Dipak Karanjavkar</div>
-                    <div className='owner-interest'>Aspiring Front End Developer</div>
-                </div>
-            </div>
-            <div className='learning-details'> 
-                <div className='haeding'>Top Areas</div>
-                <div className='expert-list'>
-                    <div className='expert-in'>Software Development Life Cycle (SDLC)</div>
-                    <div className='expert-in'>Software dev methodologies</div>
-                    <div className='expert-in'>Git Version Control, Github</div>
-                </div>
-            </div>
-        </div>
-        <div className='owner-details-card'>
-            <div className='image-name-detilas'>
-                <img className='owner-image' src={person} alt='owner-image' />
-                <div className='name-details'>
-                    <div className='owner-name'>Tejas Dipak Karanjavkar</div>
-                    <div className='owner-interest'>Aspiring Front End Developer</div>
-                </div>
-            </div>
-            <div className='learning-details'> 
-                <div className='haeding'>Top Areas</div>
-                <div className='expert-list'>
-                    <div className='expert-in'>Software Development Life Cycle (SDLC)</div>
-                    <div className='expert-in'>Software dev methodologies</div>
-                    <div className='expert-in'>Git Version Control, Github</div>
-                </div>
-            </div>
-        </div>
+            );
+        })}  
     </div>
 
     
--- src/components/recrutmentDashboard/index.js
+++ src/components/recrutmentDashboard/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import FilterOptions from '../filters'
 import OwnerDetailsCard from '../ownerDetailsCard'
 import "./recrutmentDashboard.css"
+import person from "../../assest/person.png"
 
 function RecrutmentDashboard() {
   return (
